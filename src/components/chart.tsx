346a2e355c7d50e69505575beea82f535bc863c8
Szymon Jabłoński <jablonski.szymon21@gmail.com>
2023-12-05 20:22:53 +0100

    fixed chart.tsx

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -11,34 +11,72 @@ const Chart: FC<chartParams> = ({ values, ...props }) => {
     const canvas = ref.current as HTMLCanvasElement;
     const ctx = canvas?.getContext("2d") as CanvasRenderingContext2D;
 
-    const ctxWidth = canvas.width;
-    const ctxHeight = canvas.height;
+    const xborderWidth = 50;
+    const yborderWidth = 20;
 
-    const max = Math.max(...values);
-    const min = Math.min(...values);
-    const avg = (max + min) / 2;
-
-    const upperMultiplier = ctxHeight / max - 5;
-    const lowerMultipler = (ctxHeight / 2 / min < 0 ? (ctxHeight / 2 / min) * -1 : ctxHeight / 2 / min) - 5;
-
-    const step = ctxWidth / (values.length - 1);
-
-    console.log(min, lowerMultipler);
-
-    ctx.lineWidth = 1;
+    const ctxWidth = canvas.width - xborderWidth * 2;
+    const ctxHeight = canvas.height - yborderWidth * 2;
 
     ctx.beginPath();
-    ctx.moveTo(0, 0);
 
-    values.forEach((value, index) => {
-      if (value > avg) {
-        ctx.lineTo(step * index, ctxHeight - value * upperMultiplier);
-      } else {
-        const newValue = value < 0 ? value * -1 : value;
-        ctx.lineTo(step * index, 200 + newValue * lowerMultipler);
-      }
-    });
+    ctx.fillRect(0, 0, canvas.width, yborderWidth);
+    ctx.fillRect(0, 0, xborderWidth, canvas.height);
+    ctx.fillRect(canvas.width - xborderWidth, 0, xborderWidth, canvas.height);
+    ctx.fillRect(0, canvas.height - yborderWidth, canvas.width, yborderWidth);
+
+    ctx.closePath();
+
+    const transferedValues = transferValuesToPosttive(values, Math.min(...values));
+
+    const max = Math.max(...transferedValues);
+    const min = Math.min(...transferedValues);
+
+    console.log(values);
+    console.log(transferedValues);
+
+    const chartPattern = ctxHeight / max;
+    const step = ctxWidth / (values.length - 1);
+    const beginPoint = calculatePositon(transferedValues[0], 0);
+
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(xborderWidth, beginPoint.y);
+
+    for (let i = 0; i < values.length; i++) {
+      const value1 = transferedValues[i];
+      const value2 = transferedValues[i + 1] || value1;
+
+      const { x: x1, y: y1 } = calculatePositon(value1, i);
+      const { x: x2, y: y2 } = calculatePositon(value2, i + 1);
+
+      ctx.arcTo(x1, y1, x2, y2, 15);
+
+      ctx.strokeStyle = `black`;
+
+      ctx.fillText(values[i].toString(), x1, y1 - 15);
+    }
+
     ctx.stroke();
+
+    function calculatePositon(value: number, index: number) {
+      console.log(ctxHeight - value * chartPattern);
+
+      return {
+        x: step * (index + 1),
+        y: ctxHeight - value * chartPattern + yborderWidth,
+      };
+    }
+
+    function transferValuesToPosttive(arr: number[], min: number) {
+      return arr.map(value => {
+        let newValue = 0;
+
+        if (min < 0) newValue = (min - value) / min;
+        else newValue = ((min - value) / min) * -1;
+
+        return newValue;
+      });
+    }
   }, [ref, values]);
 
   return <canvas ref={ref} {...props} />;
